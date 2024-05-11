1548e52a6162c1c6457011093acd3a24b18c42c2
Trọng Phan <dev.sytrong@gmail.com>
2024-05-11 16:33:05 +0700

    Add basic config

---
+++ Presentation/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿namespace Presentation
+{
+	/// <summary>
+	/// Functions for create dependency injections
+	/// </summary>
+	public static class DependencyInjection
+	{
+		/// <summary>
+		/// This function to add dependency injection for NuGet Package
+		/// </summary>
+		/// <param name="services"></param>
+		public static void AddPackage(this IServiceCollection services)
+		{
+			//Add other service in nuget package
+			services.AddSwaggerGen();
+		}
+
+		/// <summary>
+		/// Create dependencies for service (interface) & service (class) or repository (interface) & repository (class)
+		/// </summary>
+		/// <param name="services"></param>
+		public static void AddMasterServices(this IServiceCollection services)
+		{
+			// Add dependency injection for class and interface
+			//EX:
+			//services.AddScoped<IAccountRepository, AccountRepository>();
+			//services.AddScoped<IAccountService, AccountService>();
+		}
+	}
+
+}
