f26092eb000a419dd46323b53c0a7e63b827cf9d
tien-lai-dev <phamtannhat0@gmail.com>
2024-06-27 00:17:59 +0700

    Invoices

---
+++ Services/Mapper/InvoiceProfile.cs
@@ -0,0 +1,20 @@
+﻿using BusinessObjects.Dto.Invoices;
+using BusinessObjects.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Mapper
+{
+    public class InvoiceProfile : ProductProfile
+    {
+        public InvoiceProfile() {
+            CreateMap<OrderDetail, InvoiceResponse>()
+                .ForMember(dest => dest.CutomerInfo, src => src.MapFrom(x => x.OrderKeyNavigation.CustomerInfoKeyNavigation))
+                .ForMember(dest => dest.Product, src => src.MapFrom(x => x.ProductKeyNavigation))
+                .ReverseMap();
+        }
+    }
+}
