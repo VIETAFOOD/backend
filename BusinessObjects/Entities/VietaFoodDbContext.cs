1f162730b1ea305bb34d8d5c85b8cf21ed32d7c3
tien-lai-dev <phamtannhat0@gmail.com>
2024-07-01 18:03:34 +0700

    Fix

--- BusinessObjects/Entities/VietaFoodDbContext.cs
+++ BusinessObjects/Entities/VietaFoodDbContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 namespace BusinessObjects.Entities
 {
@@ -28,8 +29,17 @@ namespace BusinessObjects.Entities
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=(local);Database=VietaFoodDb;Uid=sa;Pwd=12345;");
+                optionsBuilder.UseSqlServer(GetConnectionString());
             }
+        }
+        private string GetConnectionString()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+            var strConn = config["ConnectionStrings:DefaultConnectionString"];
+            return strConn;
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
